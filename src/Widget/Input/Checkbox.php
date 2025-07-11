a6aba8869f9cd606f35c5a772f4e9471e0039bd5
Jhon_Walker <jbanxian@gmail.com>
2025-07-11 12:12:56 +0800

    feat: 修改Checkbox组件类继承并更新方法注释

--- src/Widget/Input/Checkbox.php
+++ src/Widget/Input/Checkbox.php
@@ -3,21 +3,19 @@ declare(strict_types=1);
 
 namespace Jiangwang\AmisSchema\Widget\Input;
 
-use Jiangwang\AmisSchema\Widget\Widget;
-
 /**
  * Checkbox 勾选框组件
- * 
+ *
  * 勾选框，用于实现勾选、不勾选操作
  *
  * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/checkbox
  *
  * @method $this option(string $value) 设置选项说明
- * @method $this trueValue(mixed $value) 设置标识真值，默认为 true
- * @method $this falseValue(mixed $value) 设置标识假值，默认为 false
- * @method $this optionType(string $value) 设置设置 option 类型，支持 default、button
+ * @method $this trueValue(string|int|bool $value=true) 设置标识真值，默认为 true
+ * @method $this falseValue(string|int|bool $value=false) 设置标识假值，默认为 false
+ * @method $this optionType(string $value='default') 设置设置 option 类型，支持 default、button
  */
-class Checkbox extends Widget
+class Checkbox extends FormItem
 {
     protected string $type = 'checkbox';
 }
