cb27061ee6b23e46ef779d4ec944b94958603a1f
Jhon_Walker <jbanxian@gmail.com>
2025-07-11 11:48:55 +0800

    feat: 增加ButtonToolbar

--- src/Widget/Input/ButtonToolbar.php
+++ src/Widget/Input/ButtonToolbar.php
@@ -3,16 +3,17 @@ declare(strict_types=1);
 
 namespace Jiangwang\AmisSchema\Widget\Input;
 
+use Jiangwang\AmisSchema\Widget\Function\Action;
 use Jiangwang\AmisSchema\Widget\Widget;
 
 /**
  * ButtonToolbar 按钮工具栏组件
- * 
- * 可以用来放置多个按钮或者按钮组，按钮之间会存在一定的间隔
+ *
+ * 默认按钮会独占一行，如果想让多个按钮并排方式，可以使用 button-toolbar 组件包裹起来，另外还有能用 button-group 来在展现上更紧凑。
  *
  * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/button-toolbar
  *
- * @method $this buttons(array $value) 设置按钮组
+ * @method $this buttons(array|Action[] $value) 设置按钮组
  */
 class ButtonToolbar extends Widget
 {
