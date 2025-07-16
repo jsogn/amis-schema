1696e4652d97247dd1367070fa439bdec48bf6c5
Jhon_Walker <jbanxian@gmail.com>
2025-07-16 15:00:03 +0800

    feat: 更新InputKV组件文档，优化方法注释，增强可读性

--- src/Widget/Input/InputKV.php
+++ src/Widget/Input/InputKV.php
@@ -6,19 +6,22 @@ namespace Jiangwang\AmisSchema\Widget\Input;
 use Jiangwang\AmisSchema\Widget\Widget;
 
 /**
- * InputKV 键值对组件
- * 
- * 用于输入键值对类型的数据
+ * InputKV 键值对
  *
- * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/input-kv
+ * 键值对输入组件，用于支持对象形式的数据编辑
  *
- * @method $this valueType(string $value) 设置值类型，支持 input、textarea
- * @method $this keyPlaceholder(string $value) 设置key 的提示信息的
- * @method $this valuePlaceholder(string $value) 设置value 的提示信息的
- * @method $this draggable(bool $value) 设置是否可拖拽排序
- * @method $this defaultValue(array $value) 设置默认值
+ * @link https://baidu.github.io/amis/zh-CN/components/form/input-kv
+ *
+ * @method $this valueType(string $value) 值类型，默认为 "input-text"
+ * @method $this keyPlaceholder(string $value) key 的提示信息
+ * @method $this valuePlaceholder(string $value) value 的提示信息
+ * @method $this draggable(bool $value) 是否可拖拽排序，默认为 true
+ * @method $this defaultValue(mixed $value) 默认值，默认为 ''
+ * @method $this autoParseJSON(bool $value) 是否自动转换 json 对象字符串，默认为 true
+ * @method $this valueSchema(array|Widget $value) 自定义 value 的 schema 配置 (3.1.0+)
+ * @method $this keySchema(array|Widget $value) 自定义 key 的 schema 配置 (3.1.0+)
  */
-class InputKV extends Widget
+class InputKV extends FormItem
 {
     protected string $type = 'input-kv';
 }
