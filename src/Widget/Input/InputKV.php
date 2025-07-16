fd275dcb3bf93fcf6330254b641633fcaa0b4d86
Jhon_Walker <jbanxian@gmail.com>
2025-07-16 15:00:43 +0800

    feat: 更新InputKV组件文档，优化方法注释，增强可读性

--- src/Widget/Input/InputKV.php
+++ src/Widget/Input/InputKV.php
@@ -12,12 +12,12 @@ use Jiangwang\AmisSchema\Widget\Widget;
  *
  * @link https://baidu.github.io/amis/zh-CN/components/form/input-kv
  *
- * @method $this valueType(string $value) 值类型，默认为 "input-text"
+ * @method $this valueType(string $value = 'input-text') 值类型，默认为 "input-text"
  * @method $this keyPlaceholder(string $value) key 的提示信息
  * @method $this valuePlaceholder(string $value) value 的提示信息
- * @method $this draggable(bool $value) 是否可拖拽排序，默认为 true
- * @method $this defaultValue(mixed $value) 默认值，默认为 ''
- * @method $this autoParseJSON(bool $value) 是否自动转换 json 对象字符串，默认为 true
+ * @method $this draggable(bool $value = true) 是否可拖拽排序，默认为 true
+ * @method $this defaultValue(mixed $value = '') 默认值，默认为 ''
+ * @method $this autoParseJSON(bool $value = true) 是否自动转换 json 对象字符串，默认为 true
  * @method $this valueSchema(array|Widget $value) 自定义 value 的 schema 配置 (3.1.0+)
  * @method $this keySchema(array|Widget $value) 自定义 key 的 schema 配置 (3.1.0+)
  */
