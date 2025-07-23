c1ffaa2a1796acedb56daf65b1c5db3521ec466d
Jhon_Walker <jbanxian@gmail.com>
2025-07-23 11:44:06 +0800

    feat: 增加组件

--- src/Widget/Input/Hidden.php
+++ src/Widget/Input/Hidden.php
@@ -3,18 +3,14 @@ declare(strict_types=1);
 
 namespace AmisSchema\Widget\Input;
 
-use AmisSchema\Widget\Widget;
-
 /**
- * Hidden 隐藏字段组件
- * 
- * 隐藏字段类型
+ * Hidden 隐藏字段
  *
- * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/hidden
+ * 默认表单提交，在没有自定义 API 请求数据的情况下，只会发送 body 里面的这些成员，对于隐藏的字段同时又希望提交表单的时候带过去，可以使用 hidden 组件
  *
- * @method $this value(mixed $value) 设置表单项值
+ * @link https://baidu.github.io/amis/zh-CN/components/form/hidden
  */
-class Hidden extends Widget
+class Hidden extends FormItem
 {
     protected string $type = 'hidden';
 }
