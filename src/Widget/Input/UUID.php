6c9b93f841107a77f379c4fd40b5b1de8f53a887
Jhon_Walker <jbanxian@gmail.com>
2025-07-23 12:10:47 +0800

    feat: 增加组件

--- src/Widget/Input/UUID.php
+++ src/Widget/Input/UUID.php
@@ -3,18 +3,16 @@ declare(strict_types=1);
 
 namespace AmisSchema\Widget\Input;
 
-use AmisSchema\Widget\Widget;
-
 /**
  * UUID 随机 ID 组件
  * 
- * 自动生成的 UUID
+ * 随机生成一个 id，可以用于防止表单重复提交。这个字段是不显示的。
  *
- * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/uuid
+ * @link https://baidu.github.io/amis/zh-CN/components/form/uuid
  *
- * @method $this length(int $value) 设置长度，默认 auto
+ * @method $this length(int $value) 设置长度，用于生成短随机数
  */
-class UUID extends Widget
+class UUID extends FormItem
 {
     protected string $type = 'uuid';
 }
