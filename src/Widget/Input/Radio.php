517837e9cb8315f9183456ee437bd7aa178131c2
Jhon_Walker <jbanxian@gmail.com>
2025-07-24 17:01:33 +0800

    feat: 增加组件

--- src/Widget/Input/Radio.php
+++ src/Widget/Input/Radio.php
@@ -3,20 +3,18 @@ declare(strict_types=1);
 
 namespace AmisSchema\Widget\Input;
 
-use AmisSchema\Widget\Widget;
-
 /**
  * Radio 单选框组件
- * 
+ *
  * 用于实现单选功能
  *
  * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/radio
  *
  * @method $this option(string $value) 设置选项说明
- * @method $this trueValue(mixed $value) 设置标识真值，默认为 true
- * @method $this falseValue(mixed $value) 设置标识假值，默认为 false
+ * @method $this trueValue(string|int|bool $value = true) 设置标识真值，默认为 true
+ * @method $this falseValue(string|int|bool $value = false) 设置标识假值，默认为 false
  */
-class Radio extends Widget
+class Radio extends FormItem
 {
     protected string $type = 'radio';
 }
