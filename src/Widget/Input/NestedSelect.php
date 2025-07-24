ff75f5583772b426344543697b2ea283fca9c5a8
Jhon_Walker <jbanxian@gmail.com>
2025-07-24 16:34:03 +0800

    feat: 增加组件

--- src/Widget/Input/NestedSelect.php
+++ src/Widget/Input/NestedSelect.php
@@ -3,34 +3,39 @@ declare(strict_types=1);
 
 namespace AmisSchema\Widget\Input;
 
-use AmisSchema\Widget\Widget;
+use AmisSchema\Widget\Function\TooltipWrapper;
 
 /**
  * NestedSelect 级联选择器组件
- * 
- * 适合选择具有级联关系的选项
  *
- * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/nestedselect
+ * 级联选择器，适合选择具有级联关系的选项
  *
- * @method $this options(array $value) 设置选项组
- * @method $this source(string|array $value) 设置动态选项组
- * @method $this delimiter(string $value) 设置拼接符，默认为逗号
- * @method $this labelField(string $value) 设置选项标签字段，默认为 label
- * @method $this valueField(string $value) 设置选项值字段，默认为 value
- * @method $this joinValues(bool $value) 设置拼接值，默认为 true
- * @method $this extractValue(bool $value) 设置提取值，默认为 false
- * @method $this cascade(bool $value) 设置设置 true时，当选中父节点时不自动选择子节点
- * @method $this withChildren(bool $value) 设置设置 true时，选中父节点时，值里面将包含子节点
- * @method $this onlyChildren(bool $value) 设置设置true时，只允许选择叶子节点
- * @method $this searchable(bool $value) 设置是否可搜索，默认为 false
- * @method $this multiple(bool $value) 设置是否多选
- * @method $this hideNodePathLabel(bool $value) 设置是否隐藏选择框中已选择节点的祖先节点文本信息
- * @method $this onlyLeaf(bool $value) 设置只允许选择叶子节点
- * @method $this clearable(bool $value) 设置是否支持清空
- * @method $this placeholder(string $value) 设置占位符
- * @method $this autoFill(array $value) 设置自动填充
+ * @link https://baidu.github.io/amis/zh-CN/components/form/nestedselect
+ *
+ * @method $this cascade(bool $value = false) 设置 true时，当选中父节点时不自动选择子节点
+ * @method $this withChildren(bool $value = false) 设置 true时，选中父节点时，值里面将包含子节点的值，否则只会保留父节点的值
+ * @method $this onlyChildren(bool $value = false) 多选时，选中父节点时，是否只将其子节点加入到值中
+ * @method $this searchPromptText(string $value = "输入内容进行检索") 搜索框占位文本
+ * @method $this noResultsText(string $value = "未找到任何结果") 无结果时的文本
+ * @method $this hideNodePathLabel(bool $value = false) 是否隐藏选择框中已选择节点的路径 label 信息
+ * @method $this onlyLeaf(bool $value = false) 只允许选择叶子节点
+ * @method $this maxTagCount(int $value) 标签的最大展示数量，超出数量后以收纳浮层的方式展示，仅在多选模式开启后生效
  */
-class NestedSelect extends Widget
+class NestedSelect extends Options
 {
     protected string $type = 'nested-select';
+
+    /**
+     * @param array{
+     *     placement: string,
+     *     trigger: string,
+     *     showArrow: bool,
+     *     offset: array
+     * } $value
+     * 收纳浮层的配置属性，详细配置参考Tooltip
+     */
+    public function overflowTagPopover(array $value): static
+    {
+        return $this->setAttribute(__FUNCTION__, $value);
+    }
 }
