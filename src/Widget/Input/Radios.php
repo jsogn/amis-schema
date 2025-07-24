65b309d8247ca0782875c74c2861aaeae1908e04
Jhon_Walker <jbanxian@gmail.com>
2025-07-24 17:02:33 +0800

    feat: 增加组件

--- src/Widget/Input/Radios.php
+++ src/Widget/Input/Radios.php
@@ -12,16 +12,9 @@ use AmisSchema\Widget\Widget;
  *
  * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/radios
  *
- * @method $this options(array $value) 设置选项组
- * @method $this source(string|array $value) 设置动态选项组
- * @method $this labelField(string $value) 设置选项标签字段，默认为 label
- * @method $this valueField(string $value) 设置选项值字段，默认为 value
  * @method $this columnsCount(int $value) 设置选项按几列来排列，默认为一列
- * @method $this inline(bool $value) 设置是否一行显示，默认为 true
- * @method $this selectFirst(bool $value) 设置是否默认选中第一个，默认为 false
- * @method $this autoFill(array $value) 设置自动填充
  */
-class Radios extends Widget
+class Radios extends Options
 {
     protected string $type = 'radios';
 }
