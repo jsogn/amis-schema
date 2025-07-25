cccbf7e0bbb19288f4b13fb52de896a2bddaafb3
Jhon_Walker <jbanxian@gmail.com>
2025-07-25 20:40:55 +0800

    feat: 增加组件

--- src/Widget/Input/InputSubForm.php
+++ src/Widget/Input/InputSubForm.php
@@ -3,29 +3,29 @@ declare(strict_types=1);
 
 namespace AmisSchema\Widget\Input;
 
-use AmisSchema\Widget\Widget;
-
 /**
  * InputSubForm 子表单组件
- * 
+ *
  * SubForm, 配置一个子表单作为当前的输入值
  *
  * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/form/input-sub-form
  *
- * @method $this multiple(bool $value) 设置是否为多选模式
- * @method $this labelField(string $value) 设置当多选时，用来展示已选项的字段
- * @method $this btnLabel(string $value) 设置按钮的文字
- * @method $this minLength(int $value) 设置限制最小选择数量
- * @method $this maxLength(int $value) 设置限制最大选择数量
+ * @method $this multiple(bool $value = false) 设置是否为多选模式，默认为 false
+ * @method $this labelField(string $value) 设置当值中存在这个字段，则按钮名称将使用此字段的值来展示
+ * @method $this btnLabel(string $value = '设置') 设置按钮默认名称，默认为 设置
+ * @method $this minLength(int $value = 0) 设置限制最小个数，默认为 0
+ * @method $this maxLength(int $value = 0) 设置限制最大个数，默认为 0
  * @method $this draggable(bool $value) 设置是否可拖拽排序
  * @method $this addable(bool $value) 设置是否可新增
  * @method $this removable(bool $value) 设置是否可删除
- * @method $this addButtonText(string $value) 设置新增时的文字
- * @method $this form(array $value) 设置子表单的表单项
- * @method $this value(mixed $value) 设置默认值
- * @method $this showErrorMsg(bool $value) 设置是否在左下角显示报错信息
+ * @method $this addButtonClassName(string $value) 设置新增按钮 CSS 类名
+ * @method $this itemClassName(string $value) 设置值元素 CSS 类名
+ * @method $this itemsClassName(string $value) 设置值包裹元素 CSS 类名
+ * @method $this form(array|Form $value) 设置子表单配置，同 Form
+ * @method $this addButtonText(string $value) 设置自定义新增一项的文本
+ * @method $this showErrorMsg(bool $value = true) 设置是否在左下角显示报错信息，默认为 true
  */
-class InputSubForm extends Widget
+class InputSubForm extends FormItem
 {
     protected string $type = 'input-sub-form';
 }
