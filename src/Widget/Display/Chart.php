c243a0805777ae30e733248512642dc6fd3100c5
Jhon_Walker <jbanxian@gmail.com>
2025-08-01 19:22:40 +0800

    feat: 增加组件

--- src/Widget/Display/Chart.php
+++ src/Widget/Display/Chart.php
@@ -10,7 +10,7 @@ use AmisSchema\Widget\Widget;
  *
  * @method $this type(string $value = 'chart') 指定为 chart 渲染器，默认 'chart'
  * @method $this body(array $value = []) 内容容器，默认 []
- * @method $this api(API|string $value = '') 配置项接口地址，默认 ''
+ * @method $this api(API|string|array $value = '') 配置项接口地址，默认 ''
  * @method $this source(string $value = '') 通过数据映射获取数据链中变量值作为配置，默认 ''
  * @method $this initFetch(bool $value = true) 组件初始化时，是否请求接口，默认 true
  * @method $this interval(int $value = 0) 刷新时间(最小 1000)，默认 0（不刷新）
@@ -20,7 +20,7 @@ use AmisSchema\Widget\Widget;
  * @method $this replaceChartOption(bool $value = false) 每次更新是完全覆盖配置项还是追加，默认 false
  * @method $this trackExpression(string $value = '') 当这个表达式的值有变化时更新图表，默认 ''
  * @method $this dataFilter(string $value = '') 自定义 echart config 转换，默认 ''
- * @method $this mapURL(API|string $value = '') 地图 geo json 地址，默认 ''
+ * @method $this mapURL(API|string|array $value = '') 地图 geo json 地址，默认 ''
  * @method $this mapName(string $value = '') 地图名称，默认 ''
  * @method $this loadBaiduMap(bool $value = false) 加载百度地图，默认 false
  * @method $this clickAction(array $value = []) 点击行为，默认 []
