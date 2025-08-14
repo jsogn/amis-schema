6732fb98368b156c4d12250f87f47fa8e9d0f5f3
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 12:25:54 +0800

    feat: 增加组件

--- src/Widget/Extra/Tasks.php
+++ src/Widget/Extra/Tasks.php
@@ -1,39 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AmisSchema\Widget\Extra;
 
+use AmisSchema\Definition\API;
 use AmisSchema\Widget\Widget;
 
 /**
  * Tasks 任务操作集合
- * 
- * 任务操作集合组件
  *
- * @method self type(string $type) 指定为 Tasks 渲染器
- * @method self className(string $className) 外层 CSS 类名
- * @method self items(array $items) 任务项配置
- * @method self checkApi(string|array $checkApi) 检查任务状态的API
- * @method self submitApi(string|array $submitApi) 提交任务的API
- * @method self reSubmitApi(string|array $reSubmitApi) 重新提交任务的API
- * @method self interval(int $interval) 轮询间隔，默认为3000毫秒
- * @method self taskNameLabel(string $taskNameLabel) 任务名称标签
- * @method self operationLabel(string $operationLabel) 操作标签
- * @method self statusLabel(string $statusLabel) 状态标签
- * @method self remarkLabel(string $remarkLabel) 备注标签
- * @method self btnText(string $btnText) 操作按钮文字
- * @method self retryBtnText(string $retryBtnText) 重试按钮文字
- * @method self btnClassName(string $btnClassName) 操作按钮CSS类名
- * @method self retryBtnClassName(string $retryBtnClassName) 重试按钮CSS类名
- * @method self statusLabelMap(array $statusLabelMap) 状态显示对应的类名映射
- * @method self statusTextMap(array $statusTextMap) 状态显示对应的文字映射
- * @method self initialStatusCode(int $initialStatusCode) 初始状态码
- * @method self readyStatusCode(int $readyStatusCode) 就绪状态码
- * @method self loadingStatusCode(int $loadingStatusCode) 进行中状态码
- * @method self canRetryStatusCode(int $canRetryStatusCode) 可重试状态码
- * @method self finishStatusCode(int $finishStatusCode) 完成状态码
- * @method self errorStatusCode(int $errorStatusCode) 错误状态码
+ * 任务操作集合，类似于 orp 上线
+ *
+ * @method $this tableClassName(string $value = '') table Dom 的类名
+ * @method $this checkApi(string|array|API $value = null) 返回任务列表，返回的数据请参考 items
+ * @method $this submitApi(string|array|API $value = null) 提交任务使用的 API
+ * @method $this reSubmitApi(string|array|API $value = null) 如果任务失败，且可以重试，提交的时候会使用此 API
+ * @method $this interval(int $value = 3000) 当有任务进行中，会每隔一段时间再次检测，时间间隔默认 3s
+ * @method $this taskNameLabel(string $value = '任务名称') 任务名称列说明
+ * @method $this operationLabel(string $value = '操作') 操作列说明
+ * @method $this statusLabel(string $value = '状态') 状态列说明
+ * @method $this remarkLabel(string $value = '备注') 备注列说明
+ * @method $this btnText(string $value = '上线') 操作按钮文字
+ * @method $this retryBtnText(string $value = '重试') 重试操作按钮文字
+ * @method $this btnClassName(string $value = 'btn-sm btn-default') 配置容器按钮 className
+ * @method $this retryBtnClassName(string $value = 'btn-sm btn-danger') 配置容器重试按钮 className
+ * @method $this statusLabelMap(array $value = ['label-warning', 'label-info', 'label-success', 'label-danger', 'label-default', 'label-danger']) 状态显示对应的类名配置
+ * @method $this statusTextMap(array $value = ['未开始', '就绪', '进行中', '出错', '已完成', '出错']) 状态显示对应的文字显示配置
  */
 class Tasks extends Widget
 {
     protected string $type = 'tasks';
+
+    /**
+     * 任务列表，每个项目包含 label、key、remark、status 属性
+     *
+     * @param array<array{
+     *     label: string,
+     *     key: string,
+     *     remark: string,
+     *     status: string
+     * }> $value 任务项数组
+     * @return $this
+     */
+    public function items(array $value = []): self
+    {
+        return $this->setAttribute(__FUNCTION__, $value);
+    }
 }
