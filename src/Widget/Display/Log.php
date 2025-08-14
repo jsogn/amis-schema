ef870785850aa480a26dfcd95b1fee6857f4212e
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 10:46:48 +0800

    feat: 增加组件

--- src/Widget/Display/Log.php
+++ src/Widget/Display/Log.php
@@ -6,21 +6,20 @@ use AmisSchema\Widget\Widget;
 
 /**
  * Log 实时日志
- * 
+ *
  * 用于实时显示日志或程序输出结果
  *
- * @method self type(string $type) 指定为 Log 渲染器
- * @method self height(int $height) 展示区域高度，默认为 500
- * @method self className(string $className) 外层 CSS 类名
- * @method self autoScroll(bool $autoScroll) 是否自动滚动，默认为 true
- * @method self disableColor(bool $disableColor) 是否禁用 ansi 颜色支持，默认为 false
- * @method self placeholder(string $placeholder) 加载中的文字
- * @method self encoding(string $encoding) 返回内容的字符编码，默认为 "utf-8"
- * @method self source(string|array $source) 接口配置
- * @method self credentials(string $credentials) fetch 的 credentials 设置，默认为 "include"
- * @method self rowHeight(int $rowHeight) 设置每行高度，将会开启虚拟渲染
- * @method self maxLength(int $maxLength) 最大显示行数
- * @method self operation(array $operation) 可选日志操作：['stop','restart','clear','showLineNumber','filter']
+ * @method $this type(string $value = 'log') 指定为 Log 渲染器，默认 'log'
+ * @method $this height(int $value = 500) 展示区域高度，默认 500
+ * @method $this autoScroll(bool $value = true) 是否自动滚动，默认 true
+ * @method $this disableColor(bool $value = false) 是否禁用 ansi 颜色支持，默认 false
+ * @method $this placeholder(string $value = '') 加载中的文字，默认 ''
+ * @method $this encoding(string $value = 'utf-8') 返回内容的字符编码，默认 'utf-8'
+ * @method $this source(string|array $value = '') 接口配置，支持变量（1.4.2+）和高级配置（1.6.1+），默认 ''
+ * @method $this credentials(string $value = 'include') fetch 的 credentials 设置，默认 'include'
+ * @method $this rowHeight(int $value = null) 设置每行高度，将会开启虚拟渲染（1.10.0+），默认 null
+ * @method $this maxLength(int $value = null) 最大显示行数（1.10.0+），默认 null
+ * @method $this operation(array $value = []) 可选日志操作：['stop','restart','clear','showLineNumber','filter']，默认 []
  */
 class Log extends Widget
 {
