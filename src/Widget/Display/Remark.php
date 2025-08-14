248210bfac4388d1cb03e2f3b9d4dd52fe08e9cc
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 11:25:29 +0800

    feat: 增加组件

--- src/Widget/Display/Remark.php
+++ src/Widget/Display/Remark.php
@@ -1,23 +1,19 @@
 <?php
-declare(strict_types=1);
 
-namespace Jiangwang\AmisSchema\Widget\Display;
+namespace AmisSchema\Widget\Display;
 
 use AmisSchema\Widget\Widget;
 
 /**
  * Remark 标记
  * 
- * 用于展示提示文本，和表单项中的 remark 属性类型。
- * @link https://aisuda.bce.baidu.com/amis/zh-CN/components/remark
+ * 用于展示提示文本，和表单项中的 remark 属性类型
  *
- * @method self type(string $type) 指定为 Remark 渲染器
- * @method self className(string $className) 外层 CSS 类名
- * @method self content(string $content) 提示文本
- * @method self placement(string $placement) 弹出位置
- * @method self trigger(string $trigger) 触发条件，默认 ['hover', 'focus']
- * @method self icon(string $icon) 图标，默认 fa fa-question-circle
- * @method self shape(string $shape) 图标形状，可选 circle、square
+ * @method $this content(string $value = '') 提示文本，支持模板变量
+ * @method $this placement(string $value = '') 弹出位置
+ * @method $this trigger(array|string $value = ['hover', 'focus']) 触发条件
+ * @method $this icon(string $value = 'fa fa-question-circle') 图标
+ * @method $this shape(string $value = 'circle') 图标形状，可选 circle、square
  */
 class Remark extends Widget
 {
