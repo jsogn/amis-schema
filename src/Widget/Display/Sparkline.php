17bd7bc9a4b6e6cc6e52ad735e5167445161bb8c
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 11:29:14 +0800

    feat: 增加组件

--- src/Widget/Display/Sparkline.php
+++ src/Widget/Display/Sparkline.php
@@ -7,15 +7,16 @@ use AmisSchema\Widget\Widget;
 /**
  * Sparkline 走势图
  * 
- * 简单的走势图组件
+ * 简单走势图，只做简单的展示，详细展示请采用 Chart 来完成
  *
- * @method self type(string $type) 指定为 Sparkline 渲染器
- * @method self className(string $className) 外层 CSS 类名
- * @method self name(string $name) 在其他组件中，时，用作变量映射
- * @method self value(array $value) 走势图数据
- * @method self width(int $width) 宽度
- * @method self height(int $height) 高度
- * @method self clickAction(string|array $clickAction) 点击行为
+ * @method $this name(string $value = '') 关联的变量，用于从环境数据中获取值
+ * @method $this value(array $value = []) 走势图数据，为数组格式
+ * @method $this width(int $value = 0) 图表宽度
+ * @method $this height(int $value = 0) 图表高度
+ * @method $this placeholder(string $value = '') 数据为空时显示的内容
+ * @method $this clickAction(array $value = []) 点击行为配置，支持 amis 的行为动作
+ * 
+ * @version 1.4.2 支持 placeholder 空数据显示功能
  */
 class Sparkline extends Widget
 {
