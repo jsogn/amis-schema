12b4aff9aec6d90c4849715c34e9996c56c3dc8e
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 10:52:20 +0800

    feat: 增加组件

--- src/Widget/Display/Number.php
+++ src/Widget/Display/Number.php
@@ -6,19 +6,17 @@ use AmisSchema\Widget\Widget;
 
 /**
  * Number 数字展示
- * 
- * 用于展示数字
  *
- * @method self type(string $type) 如果在 Table、Card 和 List 中，为"number"；在 Form 中用作静态展示，为"static-number" 或者 input-number 配置 static 属性
- * @method self className(string $className) 外层 CSS 类名
- * @method self value(string|int|float $value) 数值
- * @method self name(string $name) 在其他组件中，时，用作变量映射
- * @method self placeholder(string $placeholder) 占位内容，默认为 "-"
- * @method self kilobitSeparator(bool $kilobitSeparator) 是否千分位展示，默认为 true
- * @method self precision(int $precision) 用来控制小数点位数
- * @method self percent(bool|int $percent) 是否用百分比展示，如果是数字，还可以控制百分比小数点位数
- * @method self prefix(string $prefix) 前缀
- * @method self affix(string $affix) 后缀
+ * 用于展示数字，支持千分位、精度控制、百分比、前后缀等功能
+ *
+ * @method $this value(string|int|float $value = '') 数值，默认 ''
+ * @method $this name(string $value = '') 变量映射名称，默认 ''
+ * @method $this placeholder(string $value = '-') 占位内容，默认 '-'
+ * @method $this kilobitSeparator(bool $value = true) 是否千分位展示，默认 true
+ * @method $this precision(int $value = null) 小数点位数，默认 null
+ * @method $this percent(bool|int $value = false) 百分比展示，为数字时控制百分比小数位数，默认 false
+ * @method $this prefix(string $value = '') 前缀，默认 ''
+ * @method $this affix(string $value = '') 后缀，默认 ''
  */
 class Number extends Widget
 {
