94fd276af8dd7eaec07881ac5404f4ea79fa3f13
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 11:06:07 +0800

    feat: 增加组件

--- src/Widget/Display/Progress.php
+++ src/Widget/Display/Progress.php
@@ -7,25 +7,38 @@ use AmisSchema\Widget\Widget;
 /**
  * Progress 进度条
  *
- * @method self type(string $value) 指定为 progress 渲染器
- * @method self className(string $value) 外层CSS类名
- * @method self progressClassName(string $value) 进度条CSS类名
- * @method self value($value) 进度值
- * @method self name(string $value) 在其他组件中，时，用作变量映射
- * @method self placeholder(string $value) 占位内容
- * @method self showLabel(bool $value) 是否显示完成进度百分比
- * @method self map(array $value) 配置不同的值段，用不同的样式提示用户
- * @method self threshold($value) 阈值
- * @method self showThresholdText(bool $value) 是否显示阈值数值
- * @method self stripe(bool $value) 是否显示条纹
- * @method self animate(bool $value) 是否显示动画
- * @method self strokeWidth(int $value) 进度条线的宽度
- * @method self gapDegree(int $value) 圆形进度条缺口角度，可取值 0 ~ 295
- * @method self gapPosition(string $value) 圆形进度条缺口位置
- * @method self mode(string $value) 进度条类型，可选 line、circle、dashboard
- * @method self onEvent($value) 事件
+ * 进度条组件，支持线形、圆形、仪表盘三种模式，颜色映射、阈值、动画等功能
+ *
+ * @method $this mode(string $value = 'line') 进度条类型，line/circle/dashboard，默认 'line'
+ * @method $this value(string $value = 0) 进度值，默认 0
+ * @method $this name(string $value = '') 变量映射名称，默认 ''
+ * @method $this placeholder(string $value = '-') 占位文本，默认 '-'
+ * @method $this showLabel(bool $value = true) 是否展示进度文本，默认 true
+ * @method $this stripe(bool $value = false) 背景是否显示条纹，默认 false
+ * @method $this animate(bool $value = false) 是否支持动画(仅line类型)，默认 false
+ * @method $this map(string|array $value = ['bg-danger', 'bg-warning', 'bg-info', 'bg-success', 'bg-success']) 进度颜色映射，默认 ['bg-danger', 'bg-warning', 'bg-info', 'bg-success', 'bg-success']
+ * @method $this showThresholdText(bool $value = false) 是否显示阈值数值，默认 false
+ * @method $this valueTpl(string $value = '${value}%') 自定义格式化内容，默认 '${value}%'
+ * @method $this strokeWidth(int $value = null) 进度条线宽度，line类型默认10，circle/dashboard类型默认6，默认 null
+ * @method $this gapDegree(int $value = 75) 仪表盘缺角角度(0~295)，默认 75
+ * @method $this gapPosition(string $value = 'bottom') 仪表盘缺口位置，top/bottom/left/right，默认 'bottom'
  */
 class Progress extends Widget
 {
     protected string $type = 'progress';
+
+    /**
+     * @param array{
+     *     value: string,
+     *     color: string
+     * } | array<array{
+     *     value: string,
+     *     color: string
+     * }> $value
+     * @return self
+     */
+    public function threshold(array $value): self
+    {
+        return $this->setAttribute(__FUNCTION__, $value);
+    }
 }
