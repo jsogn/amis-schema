0d1ecb20a75311117755c88c659cd9c01704979e
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 12:34:41 +0800

    feat: 增加组件

---
+++ src/Widget/Extra/Amis.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AmisSchema\Widget\Extra;
+
+use AmisSchema\Schema;
+use AmisSchema\Widget\Widget;
+
+/**
+ * Amis 渲染器
+ * 
+ * 用于渲染数据中的 amis 配置
+ *
+ * @method $this name(string $value = '') 绑定上下文变量名
+ * @method $this props(array $value = []) 向下传递的 props，会作为默认值
+ * @method $this schema(Schema|array|string $value = null) amis 配置，可以是 JSON 对象或字符串的 JSON
+ */
+class Amis extends Widget
+{
+    protected string $type = 'amis';
+}
