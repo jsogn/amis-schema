f8b35769382b90d511b79f0d6baf86d7c663bdae
Jhon_Walker <jbanxian@gmail.com>
2025-08-14 10:37:16 +0800

    feat: 增加组件

--- src/Widget/Display/Link.php
+++ src/Widget/Display/Link.php
@@ -7,17 +7,17 @@ use AmisSchema\Widget\Widget;
 /**
  * Link 链接
  *
- * @method self type(string $value) 指定为 link 渲染器
- * @method self className(string $value) 外层CSS类名
- * @method self body($value) 标签内容
- * @method self href(string $value) 链接地址
- * @method self blank(bool $value) 是否在新标签页打开
- * @method self htmlTarget(string $value) a 标签的 target 属性
- * @method self title(string $value) a 标签的 title 属性
- * @method self disabled(bool $value) 是否禁用
- * @method self icon(string $value) 超链接图标，以加强显示
- * @method self rightIcon(string $value) 右侧图标
- * @method self onEvent($value) 事件
+ * 链接展示组件
+ *
+ * @method $this type(string $value = 'link') 如果在 Table、Card 和 List 中，为"link"；在 Form 中用作静态展示，为"static-link"，默认 'link'
+ * @method $this body(string $value = '') 标签内文本，默认 ''
+ * @method $this href(string $value = '') 链接地址，默认 ''
+ * @method $this blank(bool $value = false) 是否在新标签页打开，默认 false
+ * @method $this htmlTarget(string $value = '') a 标签的 target，优先于 blank 属性，默认 ''
+ * @method $this title(string $value = '') a 标签的 title，默认 ''
+ * @method $this disabled(bool $value = false) 禁用超链接，默认 false
+ * @method $this icon(string $value = '') 超链接图标，以加强显示，默认 ''
+ * @method $this rightIcon(string $value = '') 右侧图标，默认 ''
  */
 class Link extends Widget
 {
